4a2ac9b7617ab941a7cd3cc9034d485de4dc40a4
Debarunn1806 <debarunchakrabortty@gmail.com>
2023-10-08 01:38:29 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {ethers} from 'ethers';
+
 
 function App() {
+
+  const [connected,setConnected] =useState(false);
+  const [walletAddress,setWalletAddress] =useState("");
+
+  //to connect or disconnect wallet
+
+  const connectWallet= async ()=>{
+
+    if(!connected){
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer =await provider.getSigner();
+
+      const walletAddress=await signer.getAddress();
+
+      setConnected(true);
+      setWalletAddress(walletAddress);
+    }else{
+      window.ethereum.selectedAddress=null;
+      setConnected(false);
+      setWalletAddress("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='main'>
+        <div className='content'>
+            <button className='btn' onClick={connectWallet}>
+              {!connected ? "Connect Wallet" : "Disconnect Wallet"}
+            </button>
+
+            <h3> Address</h3>
+            <h4 className='wal-add'> {walletAddress}</h4>
+        </div>
+      </div>
     </div>
   );
 }
